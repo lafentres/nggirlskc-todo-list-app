9d3f2333563b870211135436812233cd1356f3f4
Krista LaFentres <lafentres@users.noreply.github.com>
2019-07-24 21:59:15 -0500

    Add input button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
     <h1>
       Welcome to {{ title }}!
     </h1>
+
+    <app-input-button-unit></app-input-button-unit>
   `,
   styleUrls: ['./app.component.css']
 })
