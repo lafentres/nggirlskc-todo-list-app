82715b8c8bbaf555dae7b6f678a111ad3e05f8bc
Krista LaFentres <lafentres@users.noreply.github.com>
2019-07-25 20:44:21 -0500

    Add ability to remove todo items

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
@@ -6,16 +6,25 @@ import { TodoItem } from '../interfaces/todo-item';
   template: `
     <div class="todo-item">
       {{ item.title }}
+
+      <button class="btn btn-red" (click)="removeItem()">
+        remove
+      </button>
     </div>
   `,
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  removeItem() {
+    this.remove.emit(this.item);
   }
 
 }
