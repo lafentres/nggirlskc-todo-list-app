57723d1b0d6f12b81f9992d980668338a5b6bce6
Krista LaFentres <lafentres@users.noreply.github.com>
2019-07-25 20:19:22 -0500

    Add styles to components

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -4,11 +4,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   selector: 'app-input-button-unit',
   template: `
     <input
+      class="todo-input"
       #inputElementRef
       [value]="title"
       (keyup.enter)="submitValue($event.target.value)">
 
-    <button (click)="submitValue(inputElementRef.value)">
+    <button
+      class="btn"
+      (click)="submitValue(inputElementRef.value)">
       Save
     </button>
   `,
