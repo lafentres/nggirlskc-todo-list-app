8b06b195e0f63fb1e36731ecb9f5430e1fd603bf
Krista LaFentres <lafentres@users.noreply.github.com>
2019-07-25 20:51:13 -0500

    Add ability to make an item completed

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -5,7 +5,15 @@ import { TodoItem } from '../interfaces/todo-item';
   selector: 'app-todo-item',
   template: `
     <div class="todo-item">
-      {{ item.title }}
+      <input type="checkbox"
+        class="todo-checkbox"
+        (click)="completeItem()"/>
+
+      <span
+        class="todo-title"
+        [ngClass]="{'todo-complete': item.completed}">
+        {{ item.title }}
+      </span>
 
       <button class="btn btn-red" (click)="removeItem()">
         remove
@@ -17,10 +25,18 @@ import { TodoItem } from '../interfaces/todo-item';
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
   @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
   }
 
   removeItem() {
